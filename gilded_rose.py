519751cbbcd932bd9f98cdddacbabe2db458f03c
yukki1998 <xu.lian@northeastern.edu>
2025-02-12 21:39:28 -0800

    Refactored Gilded Rose logic and updated tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -12,38 +12,88 @@ class Item:
         return "%s, %s, %s" % (self.name, self.sell_in, self.quality)
 
 
-class GildedRose(object):
+class ItemStrategy:
+    #DO NOT CHANGE THIS ATTRIBUTE!
+    def __init__(self, item):
+        self.item = item
 
-    def __init__(self, items: list[Item]):
-        # DO NOT CHANGE THIS ATTRIBUTE!!!
+    def update(self):
+        """Default update behavior for normal items"""
+        self.item.sell_in -= 1
+        self.decrease_quality()
+        if self.item.sell_in < 0:
+            self.decrease_quality()
+
+    def increase_quality(self, amount=1):
+        """Helper function to increase quality with max cap of 50"""
+        self.item.quality = min(50, self.item.quality + amount)
+
+    def decrease_quality(self, amount=1):
+        """Helper function to decrease quality with min cap of 0"""
+        if self.item.quality > 0:
+            self.item.quality -= amount
+
+
+class NormalItem(ItemStrategy):
+    """Regular item with normal degradation"""
+    pass
+
+
+class AgedBrie(ItemStrategy):
+    """Aged Brie increases in quality as time passes"""
+    def update(self):
+        self.item.sell_in -= 1
+        self.increase_quality()
+        if self.item.sell_in < 0:
+            self.increase_quality()
+
+
+class BackstagePass(ItemStrategy):
+    """Backstage passes increase in quality as concert nears, then drop to 0 after the event"""
+    def update(self):
+        self.item.sell_in -= 1
+        if self.item.sell_in < 0:
+            self.item.quality = 0
+        elif self.item.sell_in < 5:
+            self.increase_quality(3)
+        elif self.item.sell_in < 10:
+            self.increase_quality(2)
+        else:
+            self.increase_quality()
+
+
+class Sulfuras(ItemStrategy):
+    """Sulfuras, a legendary item, never decreases in quality or sell_in"""
+    def update(self):
+        pass  # Sulfuras never changes
+
+
+class ConjuredItem(ItemStrategy):
+    """Conjured items degrade twice as fast"""
+    def update(self):
+        self.item.sell_in -= 1
+        self.decrease_quality(2)
+        if self.item.sell_in < 0:
+            self.decrease_quality(2)
+
+
+class GildedRose:
+    """Main class for updating inventory items"""
+    def __init__(self, items):
         self.items = items
 
     def update_quality(self):
+        """Updates quality of all items using appropriate strategy"""
+        strategy_map = {
+            "Aged Brie": AgedBrie,
+            "Backstage passes to a TAFKAL80ETC concert": BackstagePass,
+            "Sulfuras, Hand of Ragnaros": Sulfuras,
+            "Conjured Mana Cake": ConjuredItem
+        }
+
         for item in self.items:
-            if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
-                if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros":
-                        item.quality = item.quality - 1
-            else:
-                if item.quality < 50:
-                    item.quality = item.quality + 1
-                    if item.name == "Backstage passes to a TAFKAL80ETC concert":
-                        if item.sell_in < 11:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-                        if item.sell_in < 6:
-                            if item.quality < 50:
-                                item.quality = item.quality + 1
-            if item.name != "Sulfuras, Hand of Ragnaros":
-                item.sell_in = item.sell_in - 1
-            if item.sell_in < 0:
-                if item.name != "Aged Brie":
-                    if item.name != "Backstage passes to a TAFKAL80ETC concert":
-                        if item.quality > 0:
-                            if item.name != "Sulfuras, Hand of Ragnaros":
-                                item.quality = item.quality - 1
-                    else:
-                        item.quality = item.quality - item.quality
-                else:
-                    if item.quality < 50:
-                        item.quality = item.quality + 1
+            strategy = strategy_map.get(item.name, NormalItem)
+            strategy(item).update()
+
+    def get_items(self):
+        return [item.name for item in self.items]
--- test_gilded_rose.py
+++ test_gilded_rose.py
@@ -7,13 +7,13 @@ from gilded_rose import Item, GildedRose
 class GildedRoseTest(unittest.TestCase):
     # example of test that checks for logical errors
     def test_sulfuras_should_not_decrease_quality(self):
-        items = [Item("Sulfuras", 5, 80)]
+        items = [Item("Sulfuras, Hand of Ragnaros", 5, 80)]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
         sulfuras_item = items[0]
-        self.assertEquals(80, sulfuras_item.quality)
-        self.assertEquals(4, sulfuras_item.sell_in)
-        self.assertEquals("Sulfuras", sulfuras_item.name)
+        self.assertEqual(80, sulfuras_item.quality)
+        self.assertEqual(5, sulfuras_item.sell_in)
+        self.assertEqual("Sulfuras, Hand of Ragnaros", sulfuras_item.name)
 
      # Logical Error: Aged Brie should increase in quality over time
     def test_aged_brie_quality_increases(self):
@@ -25,11 +25,19 @@ class GildedRoseTest(unittest.TestCase):
     # Logical Error: Backstage passes should increase in quality but drop to 0 after the concert
     def test_backstage_passes_quality_behavior(self):
         items = [
-            Item("Backstage passes to a TAFKAL80ETC concert", 11, 20)]
+            Item("Backstage passes to a TAFKAL80ETC concert", 11, 20),  # Increases by 1
+            Item("Backstage passes to a TAFKAL80ETC concert", 10, 20),  # Increases by 2
+            Item("Backstage passes to a TAFKAL80ETC concert", 5, 20),   # Increases by 3
+            Item("Backstage passes to a TAFKAL80ETC concert", 0, 20)    # Drops to 0
+        ]
         gilded_rose = GildedRose(items)
         gilded_rose.update_quality()
-        
-        self.assertEqual(items[0].quality, 15, "Forcing failure: Backstage passes should NOT be 15.")
+    
+        self.assertEqual(items[0].quality, 21, "Backstage passes should increase by 1 when sell_in > 10.")
+        self.assertEqual(items[1].quality, 22, "Backstage passes should increase by 2 when 10 >= sell_in > 5.")
+        self.assertEqual(items[2].quality, 23, "Backstage passes should increase by 3 when 5 >= sell_in > 0.")
+        self.assertEqual(items[3].quality, 0, "Backstage passes should drop to 0 after the concert.")
+
         
     # Logical Error: Conjured items should degrade in Quality twice as fast
     def test_conjured_items_degrade_twice_as_fast(self):
@@ -44,7 +52,7 @@ class GildedRoseTest(unittest.TestCase):
         items = [Item("Sulfuras", 5, 80)]
         gilded_rose = GildedRose(items)
         all_items = gilded_rose.get_items()
-        self.assertEquals(["Sulfuras"], all_items)
+        self.assertEqual(["Sulfuras"], all_items)
 
     # Syntax Error: GildedRose does not have a method called `update_all_items`
     def test_gilded_rose_list_all_items(self):
@@ -57,7 +65,7 @@ class GildedRoseTest(unittest.TestCase):
         except AttributeError:
             exists = False
 
-        self.assertEquals(exists, True, "Forcing failure: get_items() should exist but doesn't")
+        self.assertEqual(exists, True, "Forcing failure: get_items() should exist but doesn't")
 
 
 if __name__ == '__main__':
